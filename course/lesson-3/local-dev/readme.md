273a65fac4b5ae2814f4ab5d560dd6898b92a673
Your Name <you@example.com>
2026-03-30 16:53:44 +0800

    update local dev

--- course/lesson-3/local-dev/readme.md
+++ course/lesson-3/local-dev/readme.md
@@ -1,10 +1,19 @@
 # create a ts project according to package.json
 
+## compile
+
+```shell
+cargo build -p revive-dev-node --bin revive-dev-node --release
+cargo build -p pallet-revive-eth-rpc --bin eth-rpc --release
+```
+
 ## run node and rpc
 
 ```shell
 ./target/release/revive-dev-node --dev --tmp
-# RUST_LOG="error,evm=debug,sc_rpc_server=info,runtime::revive=debug" ./target/release/revive-dev-node --dev --tmp
+#
+RUST_LOG="error,evm=debug,sc_rpc_server=info,runtime::revive=debug" ./target/release/revive-dev-node --dev --tmp
 ./target/release/eth-rpc --dev
-# RUST_LOG="info,eth-rpc=debug" ./target/release/eth-rpc --dev
+#
+RUST_LOG="info,eth-rpc=debug" ./target/release/eth-rpc --dev
 ```
--- course/lesson-3/local-dev/src/index.ts
+++ course/lesson-3/local-dev/src/index.ts
@@ -1,13 +1,23 @@
 import { ethers, JsonRpcProvider } from "ethers";
 
-
 async function main() {
-
-    const provider = new JsonRpcProvider("http://localhost:8545");
+    const url = "http://localhost:8545";
+    const testUrl = "https://services.polkadothub-rpc.com/testnet";
+    const provider = new JsonRpcProvider(url);
     const privateKey = "0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133";
     const wallet = new ethers.Wallet(privateKey, provider);
     const address = wallet.address;
     const balance = await provider.getBalance(wallet.address);
     console.log(`Balance of ${address}: ${ethers.formatEther(balance)} ETH`);
+
+    const toAddress = "0x7072056494a815425895c743e50c37a1b232a00a";
+    const tx = await wallet.sendTransaction({
+        to: toAddress,
+        value: ethers.parseEther("0.001"),
+    });
+    console.log("tx: ", tx);
+    const receipt = await tx.wait();
+    console.log("receipt: ", receipt);
 }
+
 main();
